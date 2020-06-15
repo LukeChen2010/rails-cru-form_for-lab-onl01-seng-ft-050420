9731a3e6fffa171c1e0210f7f817d1d53be2e3bf
Luke Chen <luke.chen.2010@gmail.com>
2020-06-15 01:20:32 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,24 +1,24 @@
 class GenresController < ApplicationController
   def show
-    @genre = Artist.find(params[:id])
+    @genre = Genre.find(params[:id])
   end
   
   def new
-    @genre = Artist.new
+    @genre = Genre.new
   end
   
   def create
-	  @genre = Artist.new(artist_params)
+	  @genre = Genre.new(artist_params)
 	  @genre.save
 	  redirect_to artist_path(@genre)
 	end
 
 	def edit
-		@genre = Artist.find(params[:id])
+		@genre = Genre.find(params[:id])
 	end
 
 	def update
-	  @genre = Artist.find(params[:id])
+	  @genre = Genre.find(params[:id])
 	  @genre.update(artist_params)
 	  redirect_to artist_path(@genre)
 	end
